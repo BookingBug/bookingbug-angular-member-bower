05b9d557adf56475bcee10c0baf1a74b0862a9da
Glenn <gshoosmith@bookingbug.com>
2015-10-10 19:10:12 +0100

    Update to version: 0.0.147

--- bookingbug-angular-member.js
+++ bookingbug-angular-member.js
@@ -387,43 +387,40 @@
 }).call(this);
 
 (function() {
-  angular.module('BBMember').directive('memberForm', function($modal, $log, $rootScope, MemberLoginService, MemberBookingService) {
-    var controller, link;
-    controller = function($scope) {
-      $scope.loading = true;
-      $scope.$watch('member', function(member) {
-        if (member != null) {
-          return member.$get('edit_member').then(function(member_schema) {
-            $scope.form = member_schema.form;
-            $scope.schema = member_schema.schema;
-            return $scope.loading = false;
-          });
-        }
-      });
-      return $scope.submit = function(form) {
-        $scope.loading = true;
-        return $scope.member.$put('self', {}, form).then(function(member) {
-          $log.info("Successfully updated member");
-          return $scope.loading = false;
-        }, function(err) {
-          $log.error("Failed to update member - " + err);
-          return $scope.loading = false;
-        });
-      };
-    };
-    link = function(scope, element, attrs) {
-      var base, base1;
-      $rootScope.bb || ($rootScope.bb = {});
-      (base = $rootScope.bb).api_url || (base.api_url = attrs.apiUrl);
-      return (base1 = $rootScope.bb).api_url || (base1.api_url = "http://www.bookingbug.com");
-    };
+  angular.module('BBMember').directive('memberForm', function($modal, $log, $rootScope, MemberLoginService, MemberBookingService, AlertService, ErrorService) {
     return {
-      link: link,
-      controller: controller,
       template: "<form sf-schema=\"schema\" sf-form=\"form\" sf-model=\"member\"\n  ng-submit=\"submit(member)\" ng-hide=\"loading\"></form>",
       scope: {
         apiUrl: '@',
         member: '='
+      },
+      link: function(scope, element, attrs) {
+        var base, base1;
+        $rootScope.bb || ($rootScope.bb = {});
+        (base = $rootScope.bb).api_url || (base.api_url = attrs.apiUrl);
+        return (base1 = $rootScope.bb).api_url || (base1.api_url = "http://www.bookingbug.com");
+      },
+      controller: function($scope) {
+        $scope.loading = true;
+        $scope.$watch('member', function(member) {
+          if (member != null) {
+            return member.$get('edit_member').then(function(member_schema) {
+              $scope.form = member_schema.form;
+              $scope.schema = member_schema.schema;
+              return $scope.loading = false;
+            });
+          }
+        });
+        return $scope.submit = function(form) {
+          $scope.loading = true;
+          return $scope.member.$put('self', {}, form).then(function(member) {
+            $scope.loading = false;
+            return AlertService.raise(ErrorService.getAlert('UPDATE_SUCCESS'));
+          }, function(err) {
+            $scope.loading = false;
+            return AlertService.raise(ErrorService.getAlert('UPDATE_FAILED'));
+          });
+        };
       }
     };
   });
@@ -527,7 +524,7 @@
       };
     };
     link = function(scope, element, attrs) {
-      var base, base1, loginModal, pickCompanyModal, tryLogin;
+      var base, base1, loginModal, pickCompanyModal, session_member, tryLogin;
       $rootScope.bb || ($rootScope.bb = {});
       (base = $rootScope.bb).api_url || (base.api_url = scope.apiUrl);
       (base1 = $rootScope.bb).api_url || (base1.api_url = "http://www.bookingbug.com");
@@ -621,6 +618,10 @@
       };
       if (scope.memberEmail && scope.memberPassword) {
         return tryLogin();
+      } else if ($sessionStorage.getItem("login")) {
+        session_member = $sessionStorage.getItem("login");
+        session_member = halClient.createResource(session_member);
+        return scope.member = session_member;
       } else {
         return loginModal();
       }
